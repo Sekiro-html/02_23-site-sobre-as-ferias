fc8791ba76967d6df97379e8706814ba86efea5d
Gabriel.SK <gabrielkonrad.rs@gmail.com>
2026-02-25 09:21:09 -0300

    criação de nova pagina

---
+++ script.js
@@ -0,0 +1,12 @@
+function filterbar() {
+    var input, filter, i, TxtValue;
+    input = document.getElementsByTagName("input");
+    filter = input.value.toUpperCase();
+    table = document.getElementsByTagName("table");
+    td = document.getElementsByTagName("td");
+
+    for (i; i < td.length; i++); {
+        
+    }
+}
+
